83f6e65cfae528a166dd0e3246c03d2bc3e3ea85
Mason <bevinsm355@gmail.com>
2026-05-29 21:26:50 -0400

    Update SpeciesCard to use dynamic period colors for better visual representation

--- src/components/SpeciesCard.tsx
+++ src/components/SpeciesCard.tsx
@@ -15,7 +15,13 @@ const SpeciesCard = ({species}: SpeciesCardProps) => {
                 <h2 className="text-white font-bold text-lg">{species.commonName}</h2>
                 <p className="text-neutral-400 italic">{species.scientificName}</p>
                 <div className="flex justify-between items-center mt-2 mb-3">
-                    <p className="text-neutral-400 border border-white rounded-xl py-1 px-3 font-bold">{species.period}</p>
+                    <p  style={{
+                        color: periodColors[species.period],
+                        borderColor: periodColors[species.period],
+                        backgroundColor: periodColors[species.period] + '20' }} 
+                        className="border rounded-xl py-1 px-3 font-bold">
+                        {species.period}
+                    </p>
                     <p className="text-neutral-400">{species.lengthM}m</p>
                 </div>
                 <button className=" flex justify-center m-auto w-full p-2 border border-neutral-500 rounded-xl cursor-pointer text-white font-bold hover:bg-neutral-900 transition duration-300">+ Add</button>
